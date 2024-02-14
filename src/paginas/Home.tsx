c2b11c0276bec86dc94d835cb5a1b41efcf83525
Gabriella Vieira Bruno <gabi_bvieira@hotmail.com>
2024-02-14 14:37:49 -0300

    fazendo testes

---
+++ src/paginas/Home.tsx
@@ -0,0 +1,13 @@
+import './home/Home.css'
+
+function Home(){
+    return(
+    <>
+    <h1 className ="titulo"> Manipulando o mundo</h1>
+    <img  className = "img"src="https://i.pinimg.com/564x/d8/dc/3a/d8dc3a2fd41076ec9e52eea65ac62da0.jpg" alt= "imagem de sol"/>
+
+    </>
+    )
+}
+
+export default Home;
